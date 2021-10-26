ad7cc397743cdf0d1e5f81d98b902dabc2b6c65d
jeffamady <jeff02.jn@gmail.com>
2021-10-26 13:42:27 -0300

    passing thought project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { AddThoughtForm } from './AddThoughtForm';
+import { Thought } from './Thought';
+import { generateId, getNewExpirationTime } from './utilities';
 import './App.css';
 
 function App() {
+  const [thoughts, setThoughts] = useState([
+    {
+      id: generateId(),
+      text: 'This is a place for your passing thoughts.',
+      expiresAt: getNewExpirationTime(),
+    },
+    {
+      id: generateId(),
+      text: "They'll be removed after 15 seconds.",
+      expiresAt: getNewExpirationTime(),
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Passing Thoughts</h1>
       </header>
+      <main>
+        <AddThoughtForm />
+        <ul className="thoughts">
+          {thoughts.map((thought) => (
+            <Thought key={thought.id} thought={thought} />
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
+
 
 export default App;
